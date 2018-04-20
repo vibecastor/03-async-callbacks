13c581f790a8a60c75976ca7f69be65bbfa2d3f7
vibecastor <vibecastor@gmail.com>
2018-04-20 10:40:06 -0700

    codereview

--- lab-mike/lib/reader.js
+++ lab-mike/lib/reader.js
@@ -5,16 +5,40 @@ const logger = require('./logger');
 
 const reader = module.exports = {};
 
-reader.readFirstNCharacterAsync = (filePath, characters, callback) => {
-  logger.log(logger.VERBOSE, `Reading ${filePath}`); 
-
-  return fs.readFile(
-    filePath,
-    (error, fileBuffer) => {
-      if (error) {
-        throw error;
+// from code review...
+reader.readFiles = (paths, callback) => {
+  fs.readFile(paths[0], 'utf8', (error1, data1) => {
+    if (error1) {
+      return callback(error1); 
+    }
+    callback(data1);
+    return fs.readFile(paths[1], 'utf8', (error2, data2) => {
+      if (error2) {
+        return callback(error2);
       }
-      return callback(fileBuffer.toString('utf8', 0, characters));
-    },
-  );
-};
+      callback(data2);
+      return fs.readFile(paths[2], 'utf8', (error3, data3) => {
+        if (error3) {
+          return (error3);
+        }
+        callback(data3);
+      });
+    })
+
+  })
+}
+
+
+// reader.readFirstNCharacterAsync = (filePath, characters, callback) => {
+//   logger.log(logger.VERBOSE, `Reading ${filePath}`); 
+
+//   return fs.readFile(
+//     filePath,
+//     (error, fileBuffer) => {
+//       if (error) {
+//         throw error;
+//       }
+//       return callback(fileBuffer.toString('utf8', 0, characters));
+//     },
+//   );
+// };
